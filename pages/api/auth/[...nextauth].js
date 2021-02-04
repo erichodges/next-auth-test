e86b9d89fdedcc89a35b9c9b241a23f12a3888cf
Eric Hodges <ehodges2004@gmail.com>
2021-02-03 17:19:33 -0800

    Added next-auth and code to setup github oauth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+// Everything in the api folder runs on the server
+
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default (req, res) =>
+  NextAuth(req, res, {
+    providers: [
+      Providers.GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+      })
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    secret: process.env.AUTH_SECRET,
+    jwt: {
+      secret: process.env.JWT_SECRET
+    }
+  });
